93bb043bee449e1170b661c3c58b18e2b121388c
LMyang <liumy@eptison.com>
2019-08-20 15:06:19 +0800

    init

---
+++ ribbon-demo1/hello-service/src/main/java/com/example/springcloud/helloservice/HelloServiceApplication.java
@@ -0,0 +1,20 @@
+package com.example.springcloud.helloservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * HelloService
+ *
+ * @author LMyang
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class HelloServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloServiceApplication.class, args);
+    }
+
+}
